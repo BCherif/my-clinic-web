e8ebcaa22e8ba9f1c3981e5da2da09c4f3fa447b
mlkomou <malifoli.musique@gmail.com>
2022-08-29 08:10:08 +0000

    vues patient and doctor

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {Role} from "../core/models/role";
 
 const routes: Routes = [
   {
@@ -10,6 +11,21 @@ const routes: Routes = [
     path: 'type-consultation',
     loadChildren: () =>
       import('./type-consultation/type-consultation.module').then((m) => m.TypeConsultationModule),
+  },
+  {
+    path: "doctors",
+    loadChildren: () =>
+      import("./doctors/doctors.module").then((m) => m.DoctorsModule),
+  },
+  {
+    path: "staff",
+    loadChildren: () =>
+      import("./staff/staff.module").then((m) => m.StaffModule),
+  },
+  {
+    path: "patients",
+    loadChildren: () =>
+      import("./patients/patients.module").then((m) => m.PatientsModule),
   },
 ];
 
